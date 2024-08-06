db8837e7eabb0b61048457d7557acd5b831ae36d
manasswarnkar <manasswarnkar0@gmail.com>
2024-08-07 00:52:08 +0530

    reading qr passed

--- reading-qr/reading_qr/src/main.rs
+++ reading-qr/reading_qr/src/main.rs
@@ -12,12 +12,12 @@ struct QrCode {
     code: String,
 }
 
-async fn get_img(access_token: &str) -> Result<(), Box<dyn error::Error>> {
+async fn get_img(access_token: &str) -> Result<Mat, Box<dyn error::Error>> {
     let str = format!(
         "https://hackattic.com/challenges/reading_qr/problem?access_token={}",
         access_token
     );
-    let mut resp = reqwest::get(str.as_str())
+    let mut resp = reqwest::get(&str)
         .await?
         .json::<HashMap<String, String>>()
         .await?;
@@ -31,9 +31,12 @@ async fn get_img(access_token: &str) -> Result<(), Box<dyn error::Error>> {
     let img2 = ImageReader::new(Cursor::new(bytes))
         .with_guessed_format()?
         .decode()?;
-    img2.save("qrcode.png")?;
-    println!("Image downloaded and saved as qrcode.png");
-    Ok(())
+    let mut buffer = Vec::new();
+    img2.write_to(&mut Cursor::new(&mut buffer), image::ImageFormat::Png)?;
+    
+    let mat = imgcodecs::imdecode(&opencv::core::Mat::from_slice(&buffer)?, imgcodecs::IMREAD_COLOR)?;
+    println!("get_img success");
+    Ok(mat)
 }
 
 #[tokio::main]
@@ -42,9 +45,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let access_token: String =
         env::var("ACCESS_TOKEN").expect("ACCESS_TOKEN must be set in env file.");
 
-    let _res = get_img(&access_token);
-
-    let img = imgcodecs::imread("./qrcode.png", imgcodecs::IMREAD_COLOR)?;
+    let img = get_img(&access_token).await?;
     let mut gray = Mat::default();
     imgproc::cvt_color(&img, &mut gray, imgproc::COLOR_BGR2GRAY, 0)?;
 
@@ -70,8 +71,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("Decoded text: {:?}", decoded_text);
 
             let qr_code = QrCode { code: decoded_text };
-            let json_string = serde_json::to_string(&qr_code)?;
-            println!("json_string: {}", json_string);
+            // let json_string = serde_json::to_string(&qr_code)?;
+            // println!("json_string: {}", json_string);
 
             let client = reqwest::Client::new();
             let post_url = format!(
@@ -80,12 +81,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             );
             let post_response = client
                 .post(post_url.as_str())
-                .json(&json_string)
+                .json(&qr_code)
                 .send()
                 .await?;
 
             if post_response.status().is_success() {
-                println!("Success: {:?}", post_response.status());
+                println!("Success: {}", post_response.text().await?);
             } else {
                 println!("Failed to post JSON data: {:?}", post_response.status());
             }
